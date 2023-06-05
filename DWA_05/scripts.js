91d4fb6692c210b4ddeee8599150650aa6e7a1b1
jarreed <jarreedw@gmail.com>
2023-06-05 17:19:40 +0200

    modified js file

--- DWA_05/scripts.js
+++ DWA_05/scripts.js
@@ -3,10 +3,11 @@ const result = document.querySelector("[data-result]");
 const body = document.querySelector("body")
 
 form.addEventListener("submit", (event) => {
-  event.preventDefault();
+	event.preventDefault();
 
-  const entries = new FormData(event.target);
-  const { dividend, divider } = Object.fromEntries(entries);
+	const entries = new FormData(event.target);
+	const { dividend, divider } = Object.fromEntries(entries);
+
 
 	if (dividend === "" || divider === "") {
 		result.innerText = "Division not performed. Both values are required in inputs. Try again"
@@ -15,12 +16,12 @@ form.addEventListener("submit", (event) => {
 		result.innerText = "Division not performed. Invalid number provided. Try again"
 		throw new Error("Division not performed. Invalid number provided. Try again");
 
-	} else if (dividend !== isNaN || divider !== isNaN) {
+	} else if (isNaN(dividend) || isNaN(divider)) {
 		body.innerText = "Something critical went wrong. Please reload the page"
 		throw new Error("Something critical went wrong. Please reload the page");
-		
+
 	} else {
 		result.innerText = Math.floor(dividend / divider);
 	}
-  
+
 });
