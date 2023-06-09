c2ad1a979a3865aee66c4f0c4ee4ac7b06599cd8
jarreed <jarreedw@gmail.com>
2023-06-09 23:33:36 +0200

    modified js file

--- DWA_05/scripts.js
+++ DWA_05/scripts.js
@@ -1,6 +1,5 @@
 const form = document.querySelector("[data-form]");
 const result = document.querySelector("[data-result]");
-const body = document.querySelector("body")
 
 form.addEventListener("submit", (event) => {
 	event.preventDefault();
@@ -8,17 +7,21 @@ form.addEventListener("submit", (event) => {
 	const entries = new FormData(event.target);
 	const { dividend, divider } = Object.fromEntries(entries);
 
+	const dividendAsNum = parseInt(dividend);
+	const dividerAsNum = parseInt(divider);
 
 	if (dividend === "" || divider === "") {
 		result.innerText = "Division not performed. Both values are required in inputs. Try again"
 
-	} else if (divider < 0) {
+	} else if (isNaN(dividend) || isNaN(divider)) {
+		document.body.innerHTML = /* html */ `
+		Something critical went wrong. Please reload the page.
+		`;
+		throw new Error("Something critical went wrong. Please reload the page");
+
+	} else if (dividerAsNum < 0) {
 		result.innerText = "Division not performed. Invalid number provided. Try again"
 		throw new Error("Division not performed. Invalid number provided. Try again");
-
-	} else if (isNaN(dividend) || isNaN(divider)) {
-		body.innerText = "Something critical went wrong. Please reload the page"
-		throw new Error("Something critical went wrong. Please reload the page");
 
 	} else {
 		result.innerText = Math.floor(dividend / divider);
