f9dec1f564c0e9783418b2f0e9ebb9b39ed91d01
jarreed <jarreedw@gmail.com>
2023-06-22 04:23:23 +0200

    created web component, added more functionality

--- DWA_10/scripts.js
+++ DWA_10/scripts.js
@@ -1,23 +1,52 @@
+const MAX_NUMBER = 20
+const MIN_NUMBER = -20
 STEP_AMOUNT = 1
 
-const number = document.querySelector('[data-key="number"]')
-const subtract = document.querySelector('[data-key="subtract"]')
-const add = document.querySelector('[data-key="add"]')
-const reset = document.querySelector('[data-key="reset"]')
-const resetmsg = document.querySelector('[data-key="alert-reset"]');
+const number = document.querySelector('[data-value-number]')
+const subtract = document.querySelector('[data-value-subtract]')
+const add = document.querySelector('[data-value-add]')
+const reset = document.querySelector('[data-reset-button]')
+const resetmsg = document.querySelector('[data-rest-msg]');
 
 const increment = () => {
     const newValue = parseInt(number.value) + STEP_AMOUNT
     number.value = newValue
+
+    // Enable subtract button
+    if (subtract.disabled === true) {
+        subtract.disabled = false
+    }
+
+    // Disable add button when number is bigger or equal to max value
+    if (newValue >= MAX_NUMBER) {
+        add.disabled = true
+    }
 }
 
 const decrement = () => {
     const newValue = parseInt(number.value) - STEP_AMOUNT
     number.value = newValue
+
+    // Enable add button
+    if (add.disabled === true) {
+        add.disabled = false
+    }
+
+    // Disable subtract button when number is smaller or equal to min value
+    if (newValue <= MIN_NUMBER) {
+        subtract.disabled = true
+    }
 }
 
 const Reset = () => {
     number.value = 0
+
+    // Enable subtract and add buttons
+    if (subtract.disabled === true || add.disabled === true) {
+        subtract.disabled = false
+        add.disabled = false
+    }
+
     resetmsg.show()
 }
 
